904ef4f3424f4638cd39765a4d337ed750727f92
Ezenwa Hopekell <hopekelltech@gmail.com>
2022-06-28 12:03:01 +0100

    First Publishing

---
+++ index.php
@@ -0,0 +1,34 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Email Scheduler Login</title>
+    <link rel="stylesheet" href="/assets/css/bootstrap.css" />
+    <link rel="stylesheet" href="/assets/css/style.css" />
+</head>
+<body>
+    <div class="container">
+        <div class="auth-box">
+            <h3 class="text-info text-center">App Login</h3>
+            <form action="" method="post">
+                <div class="form-group space-sm">
+                    <label for="email">Enter Email:</label>
+                    <input type="email" class="form-control" name="email" placeholder="hopekelltech@gmail.com" required />
+                </div>
+                <div class="form-group space-sm">
+                    <label for="password">Password:</label>
+                    <input type="password" class="form-control" name="password" placeholder="%hU8949/.dG" required />
+                </div>
+                <div class="form-group d-grid">
+                    <button class="btn btn-info" type="submit" name="btn-login">Login</button>
+                </div>
+            </form>
+        </div>
+    </div>
+
+
+    <script src="/assets/js/bootstrap.js"></script>
+</body>
+</html>
